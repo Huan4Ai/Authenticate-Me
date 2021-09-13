48a0b7bfc9cdbd17500a0514f7d64734a35ceb67
Huan Ai <aihuan96@gmail.com>
2021-09-13 16:21:58 -0400

    seeded some data for questions

---
+++ backend/db/seeders/20210913194753-add-questions.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.bulkInsert('Questions', [
+      {
+        "ownerId": 1,
+        "title": "What are some unknown facts about dogs?",
+        "description": "As human's best friends, many people love dogs. I love dogs and I want to learn about them too! Can someone give me more details about dogs?",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        "ownerId": 2,
+        "title": "What are the benefits of getting a dog as a pet?",
+        "description": "I have seen many of my friends having a dog as pet. What are the benefits of owning a dog?",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+
+     ], {});
+
+      },
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.dropTable('Questions');
+  }
+};
