c0a2ac66f3bf12ecddf3494bc82610fec62f3830
Huan Ai <aihuan96@gmail.com>
2021-09-13 19:44:49 -0400

    did get routes for questions

--- backend/routes/api/questions.js
+++ backend/routes/api/questions.js
@@ -7,3 +7,37 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+
+//get all questions in the database
+router.get('/', asyncHandler(async (req, res) => {
+  const allQuestions = await Question.findAll({
+    include: User
+  });
+
+  return res.json({
+    allQuestions,
+  });
+
+}),
+
+);
+
+//get a specific question in the database
+router.get('/:id(\\d+)', asyncHandler(async (req, res, next) => {
+  const specificQuestion = await Question.findByPk(req.params.id, {
+    include: User
+  });
+
+  if (specificQuestion) {
+    return res.json({
+      specificQuestion
+    })
+  } else {
+    next(new Error("Question not found"));
+  };
+
+}),
+
+);
+
+router.post('/', csrf)
