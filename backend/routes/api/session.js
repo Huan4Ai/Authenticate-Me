1cb641056a2c2ecf553ccfcae47c867fb73c58db
Huan Ai <aihuan96@gmail.com>
2021-09-13 12:53:54 -0400

    added delete api route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,85 +1,72 @@
-const express = require('express');
+const express = require('express')
 const asyncHandler = require('express-async-handler');
-
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
-
-const router = express.Router();
-
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
+const router = express.Router();
 const validateLogin = [
-  check('credential')
-    .exists({ checkFalsy: true })
-    .notEmpty()
-    .withMessage('Please provide a valid email or username.'),
-  check('password')
-    .exists({ checkFalsy: true })
-    .withMessage('Please provide a password.'),
-  handleValidationErrors,
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors,
 ];
 
+
+
+// User login api route
 router.post(
-  '/',
-  asyncHandler(async (req, res, next) => {
-    const { credential, password } = req.body;
+    '/',
+    validateLogin,
+    asyncHandler(async (req, res, next) => {
+      const { credential, password } = req.body;
 
-    const user = await User.login({ credential, password });
+      const user = await User.login({ credential, password });
 
-    if (!user) {
-      const err = new Error('Login failed');
-      err.status = 401;
-      err.title = 'Login failed';
-      err.errors = ['The provided credentials were invalid.'];
-      return next(err);
-    }
+      if (!user) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = ['The provided credentials were invalid.'];
+        return next(err);
+      }
 
-    await setTokenCookie(res, user);
+      await setTokenCookie(res, user);
 
-    return res.json({
-      user,
-    });
-  }),
-);
-
-router.get(
-  '/',
-  restoreUser,
-  (req, res) => {
-    const { user } = req;
-    if (user) {
       return res.json({
-        user: user.toSafeObject()
+        user,
       });
-    } else return res.json({});
-  }
+    }),
 );
 
-
-router.post(
-  '/',
-  validateLogin,
-  asyncHandler(async (req, res, next) => {
-    const { credential, password } = req.body;
-
-    const user = await User.login({ credential, password });
-
-    if (!user) {
-      const err = new Error('Login failed');
-      err.status = 401;
-      err.title = 'Login failed';
-      err.errors = ['The provided credentials were invalid.'];
-      return next(err);
+// Log out: asyncHandler wasn't used to wrap the route handler. This is because the route handler is not async.
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
     }
-
-    await setTokenCookie(res, user);
-
-    return res.json({
-      user,
-    });
-  }),
 );
+
+// Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+);
+
 
 
 module.exports = router;
