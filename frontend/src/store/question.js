05235fe2da983f4daadfcc2c42801babb263e9e0
Huan Ai <aihuan96@gmail.com>
2021-09-14 18:39:23 -0400

    before working on reducer

--- frontend/src/store/question.js
+++ frontend/src/store/question.js
@@ -9,10 +9,14 @@ const load = list => ({
   list,
 });
 
-const addOneQuestion = ({
+const addOneQuestion = question => ({
   type: ADD_ONE,
   question,
 });
+
+const removeQuestion = () => ({
+  type:REMOVE_QUESTION,
+})
 
 export const getQuestion = () => async dispatch => {
   const response = await fetch('/api/questions');
@@ -32,4 +36,39 @@ export const getSingleQuestion = (questionId) => async dispatch => {
     dispatch(addOneQuestion(question));
   }
 
+};
+export const createAQuestion = (questionData) => async dispatch => {
+  const response = await fetch(`/api/questions`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(questionData),
+  });
+  if (response.ok) {
+    const question = await response.json();
+    dispatch(addOneQuestion(question));
+    return question;
+  }
+
+};
+
+export const editAQuestion = (questionData) => async dispatch => {
+  const response = await fetch(`/api/questions/${questionData.id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(questionData),
+  });
+  if (response.ok) {
+    const question = await response.json();
+    dispatch(addOneQuestion(question));
+    return question;
+  }
+
+};
+
+export const removeAQuestion = (questionData) => async dispatch => {
+  const response = await fetch(`/api/questions/${questionData.id}`, {
+    method: 'DELETE',
+  });
+  dispatch(removeQuestion());
+  return response;
 };
