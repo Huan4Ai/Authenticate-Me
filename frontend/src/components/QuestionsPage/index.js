b56813e16183641d3e57adf4e6389286a1942a1a
Huan Ai <aihuan96@gmail.com>
2021-09-15 16:08:12 -0400

    started all questions page

--- frontend/src/components/QuestionsPage/index.js
+++ frontend/src/components/QuestionsPage/index.js
@@ -1,5 +1,26 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import {}
+import { getQuestion } from "../../store/question";
+
+function ShowAllQuestions() {
+  const dispatch = useDispatch();
+  const questions = useSelector(state => state.questions);
+
+  useEffect(() => {
+    dispatch(getQuestion())
+  }, [dispatch]);
+
+
+  return (
+    <ul>{questions.map(question => <li>{question}</li>) }</ul>
+  )
+
+
+
+
+
+}
+
+export default ShowAllQuestions;
