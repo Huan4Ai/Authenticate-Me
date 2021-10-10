533a3f073493e200544a37ad85668675a20bd8b0
Huan Ai <aihuan96@gmail.com>
2021-10-10 14:12:57 -0400

    imported google font

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -26,37 +26,45 @@ function LoginForm() {
       <div className="title_form_container">
         <div className="title_description_container">
           <div className="doguora_title">
-            <img src={logo} alt="icon" />
+            <h1>Doguora</h1>
           </div>
           <div className="title_description">
             <p>A place to share knowledge and better understand dogs</p>
           </div>
         </div>
         <form onSubmit={handleSubmit} className="login_form">
-          <ul>
-            {errors.map((error, idx) => (
-              <li key={idx}>{error}</li>
-            ))}
-          </ul>
-          <label>
-            Username or Email
-            <input
-              type="text"
-              value={credential}
-              onChange={(e) => setCredential(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            Password
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </label>
-          <button type="submit">Log In</button>
+          <div className="login_errors">
+            <ul>
+              {errors.map((error, idx) => (
+                <li key={idx}>{error}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="username">
+            <label>
+              Username or Email
+              <input
+                type="text"
+                value={credential}
+                onChange={(e) => setCredential(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+          <div className="password">
+            <label>
+              Password
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+          <div className="login_button">
+            <button type="submit">Log In</button>
+          </div>
         </form>
       </div>
     </div>
