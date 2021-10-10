0947cd716ff7919255e8328c2fc70647b3ae99ed
Huan Ai <aihuan96@gmail.com>
2021-10-09 23:11:36 -0400

    before yexiao

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
+import "./LoginForm.css"
+import logo from '../../images/logo1.png'
 
 function LoginForm() {
   const dispatch = useDispatch();
@@ -20,32 +22,44 @@ function LoginForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => (
-          <li key={idx}>{error}</li>
-        ))}
-      </ul>
-      <label>
-        Username or Email
-        <input
-          type="text"
-          value={credential}
-          onChange={(e) => setCredential(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    <div className="login_container">
+      <div className="title_form_container">
+        <div className="title_description_container">
+          <div className="doguora_title">
+            <img src={logo} alt="icon" />
+          </div>
+          <div className="title_description">
+            <p>A place to share knowledge and better understand dogs</p>
+          </div>
+        </div>
+        <form onSubmit={handleSubmit} className="login_form">
+          <ul>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
+            ))}
+          </ul>
+          <label>
+            Username or Email
+            <input
+              type="text"
+              value={credential}
+              onChange={(e) => setCredential(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          <button type="submit">Log In</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
