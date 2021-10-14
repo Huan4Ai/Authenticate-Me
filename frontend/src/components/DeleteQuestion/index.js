c937096bdfc95e2432a868ca4f5e81b5afda7dcb
Huan Ai <aihuan96@gmail.com>
2021-10-13 23:01:55 -0400

    added cascade hook

--- frontend/src/components/DeleteQuestion/index.js
+++ frontend/src/components/DeleteQuestion/index.js
@@ -1,23 +1,26 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { removeAQuestion } from "../../store/question";
+import { useHistory } from "react-router-dom";
 
 function DeleteSingleQuestion() {
   const dispatch = useDispatch();
   const { questionId } = useParams();
   const singleQuestion = useSelector(state => state.question[questionId]);
+  const history = useHistory();
 
   const handleDelete = (e) => {
     e.preventDefault();
     dispatch(removeAQuestion(questionId));
+    history.push("/")
   }
 
 
   if (singleQuestion !== null || singleQuestion !== undefined) {
     return (
-      <div>
-        <button onClick={handleDelete}>Delete</button>
-      </div>
+        <form onSubmit={handleDelete}>
+          <button type="submit">Delete</button>
+        </form>
 
 
   );
