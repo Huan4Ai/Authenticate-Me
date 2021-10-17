d474b7cc899b6db029ae1219f40baa2893cb63ea
Huan Ai <aihuan96@gmail.com>
2021-10-16 20:11:40 -0400

    styled add question button

--- frontend/src/components/CreateQuestionPage/index.js
+++ frontend/src/components/CreateQuestionPage/index.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import CreateSingleQuestion from './CreateQuestion';
+import "./CreateQuestion.css"
 function CreateQuestionModal() {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <button onClick={() => setShowModal(true)}>Add question</button>
+      <button onClick={() => setShowModal(true)} className="addQuestionButton">Add question</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <CreateSingleQuestion setShowModal={setShowModal} />
