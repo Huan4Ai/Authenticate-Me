0532565a95ae04d973985e324f5e20e9b4abb972
Huan Ai <aihuan96@gmail.com>
2021-10-17 17:09:04 -0400

    centered the question page

--- frontend/src/components/QuestionsPage/index.js
+++ frontend/src/components/QuestionsPage/index.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { getQuestion } from "../../store/question";
 import { Link } from "react-router-dom";
+import "./QuestionsPage.css"
 
 function ShowAllQuestions() {
   const dispatch = useDispatch();
@@ -14,13 +15,24 @@ function ShowAllQuestions() {
   }, [dispatch]);
 
   return (
-    <ul>{Object.keys(questions).map(key =>
-        <li key={questions[key].id}>
-        <Link to={`/questions/${questions[key].id}`}>
-          <div>{questions[key].title}</div>
-        </Link>
-        </li>)}
-    </ul>
+    // <ul>{Object.keys(questions).map(key =>
+    //     <li key={questions[key].id}>
+    //     <Link to={`/questions/${questions[key].id}`}>
+    //       <div>{questions[key].title}</div>
+    //     </Link>
+    //     </li>)}
+    // </ul>
+
+
+    	<div className="questionsContainer">
+				{Object.keys(questions).map((question) => (
+					<Link key={questions[question].id} to={`/questions/${questions[question].id}`}>
+						<div className="questionsLinks">
+							<p> {questions[question].title} </p>
+						</div>
+					</Link>
+				))}
+			</div>
   );
 
   // return null;
