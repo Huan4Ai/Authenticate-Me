3d04f5bb6caf84601c967cf992de5dd55fdafa87
Huan Ai <aihuan96@gmail.com>
2021-10-16 20:20:07 -0400

    nav logo now works

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
-import logo from '../../images/android-chrome-192x192.png';
+import logo from '../../images/customLogo2.jpg';
 import CreateQuestionModal from '../CreateQuestionPage';
 
 
