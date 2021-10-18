73b4188fb3cddafef07d48da90c299f7e09016e4
Huan Ai <aihuan96@gmail.com>
2021-10-18 10:04:11 -0400

    styled delete question on question

--- frontend/src/components/DeleteQuestion/DeleteQuestion.js
+++ frontend/src/components/DeleteQuestion/DeleteQuestion.js
@@ -25,16 +25,18 @@ function DeleteSingleQuestion({ setShowModal }) {
 
   if (singleQuestion !== null || singleQuestion !== undefined) {
     return (
-      <form onSubmit={handleDelete}>
-        <div>
-          <h1>Delete a question about dogs</h1>
-          <p>Are you sure you want to delete this question?</p>
-        </div>
-        <div>
-          <button type="submit">Delete</button>
-          <button type="button" onClick={handleCancelClick}>Cancel</button>
-        </div>
-      </form>
+      <div className="deleteQuestionContainer">
+        <form onSubmit={handleDelete}>
+          <div>
+            <h3>Delete a question about dogs</h3>
+            <p>Are you sure you want to delete this question?</p>
+          </div>
+          <div>
+            <button type="submit" className="deleteButton">Delete</button>
+            <button type="button" onClick={handleCancelClick} className="cancelButton">Cancel</button>
+          </div>
+        </form>
+      </div>
   );
 
   }
