9b45593ea899b7c1ded6d6c4a16cbf9b3d4e754d
Huan Ai <aihuan96@gmail.com>
2021-10-18 09:57:23 -0400

    styled edit question on question

--- frontend/src/components/EditQuestionPage/EditQuestion.js
+++ frontend/src/components/EditQuestionPage/EditQuestion.js
@@ -37,19 +37,26 @@ function EditSingleQuestion({ setShowModal }) {
 
   if (singleQuestion !== null || singleQuestion !== undefined) {
     return (
-      <div>
-        <h2>Edit a question about dogs</h2>
+      <div className="editQuestionContainer">
+        <h3>Edit a question about dogs</h3>
         <form onSubmit={handleEdit}>
           <div>
             <label htmlFor='title'>Title:</label>
-            <input id='title' type='text' onChange={(e) => setTitle(e.target.value)} value={title} required />
+          </div>
+          <div>
+            <input id='title' type='text' onChange={(e) => setTitle(e.target.value)} value={title} required className="inputField" />
           </div>
           <div>
             <label htmlFor='description'>Description:</label>
-            <textarea id='description' type='text' onChange={(e) => setDescription(e.target.value)} value={description} required />
           </div>
-          <button type="submit">Submit</button>
-          <button type="button" onClick={handleCancelClick}>Cancel</button>
+          <div>
+            <textarea id='description' type='text' onChange={(e) => setDescription(e.target.value)} value={description} required className="inputField" />
+          </div>
+          <div>
+            <button type="submit" className="submitButton">Submit</button>
+            <button type="button" onClick={handleCancelClick} className="cancelButton">Cancel</button>
+          </div>
+
         </form>
       </div>
 
