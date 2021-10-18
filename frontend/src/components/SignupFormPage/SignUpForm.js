8e6dbb335317df3475ec858fbb9db377930abcf7
Huan Ai <aihuan96@gmail.com>
2021-10-17 21:09:52 -0400

    styled sign up form

--- frontend/src/components/SignupFormPage/SignUpForm.js
+++ frontend/src/components/SignupFormPage/SignUpForm.js
@@ -29,48 +29,72 @@ function SignupFormPage() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
-      <label>
-        Email
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Username
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Confirm Password
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Sign Up</button>
-    </form>
+    <div className="signUpContainer">
+      <form onSubmit={handleSubmit}>
+        <ul>
+          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+        </ul>
+        <div>
+          <label>
+            Email
+          </label>
+        </div>
+        <div>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            required
+            className="inputField"
+            />
+        </div>
+        <div>
+          <label>
+            Username
+          </label>
+        </div>
+        <div>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            required
+            className="inputField"
+            />
+        </div>
+        <div>
+          <label>
+            Password
+          </label>
+        </div>
+        <div>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            required
+            className="inputField"
+            />
+        </div>
+        <div>
+          <label>
+            Confirm Password
+          </label>
+        </div>
+        <div>
+            <input
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+              className="inputField"
+            />
+        </div>
+        <div>
+          <button type="submit" className="signUpButton">Sign Up</button>
+        </div>
+      </form>
+    </div>
   );
 }
 
