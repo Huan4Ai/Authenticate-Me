0a6a86bb97278c2747741d66e8fb70e75881b938
Huan4Ai <aihuan96@gmail.com>
2021-12-23 17:30:09 -0500

    found the correct logo

--- frontend/src/components/SplashLogin/HomePage.js
+++ frontend/src/components/SplashLogin/HomePage.js
@@ -1,22 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { NavLink, Redirect } from "react-router-dom";
 import { useSelector } from 'react-redux';
+import Doguora from "./Doguora2.png"
+import "./SplashLanding.css"
 
 
 function SplashLanding() {
   return (
     <>
-      <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-          Dropdown button
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-          <li><a class="dropdown-item" href="#">Action</a></li>
-          <li><a class="dropdown-item" href="#">Another action</a></li>
-          <li><a class="dropdown-item" href="#">Something else here</a></li>
-        </ul>
-      </div>
-
+      <img id="newLogo" src={Doguora}></img>
     </>
 
 
