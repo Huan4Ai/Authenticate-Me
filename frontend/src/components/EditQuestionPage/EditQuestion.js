1dc466adc672f5fedcec313ceafe15fb896949be
Huan4Ai <aihuan96@gmail.com>
2021-12-28 11:17:45 -0500

    started redo edit modal

--- frontend/src/components/EditQuestionPage/EditQuestion.js
+++ frontend/src/components/EditQuestionPage/EditQuestion.js
@@ -11,8 +11,6 @@ function EditSingleQuestion({ singleQuestion, onClose }) {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
 
-  // console.log(questionId)
-
 
   const handleEdit = async (e) => {
     e.preventDefault();
@@ -35,28 +33,25 @@ function EditSingleQuestion({ singleQuestion, onClose }) {
   };
 
   return (
-    <div className="editQuestionContainer">
-      <h3>Edit a question about dogs</h3>
-      <form onSubmit={handleEdit}>
-        <div>
-          <label htmlFor='title'>Title:</label>
-        </div>
-        <div>
-          <input id='title' type='text' onChange={(e) => setTitle(e.target.value)} value={title} required className="inputField" />
-        </div>
-        <div>
-          <label htmlFor='description'>Description:</label>
-        </div>
-        <div>
-          <textarea id='description' type='text' onChange={(e) => setDescription(e.target.value)} value={description} required className="inputField" />
-        </div>
-        <div>
-          <button type="submit" className="submitButton">Submit</button>
-          <button type="button" onClick={handleCancelClick} className="cancelButton">Cancel</button>
-        </div>
+    <form onSubmit={handleEdit}>
+      <div>
+        <label htmlFor='title'>Title:</label>
+      </div>
+      <div>
+        <input id='title' type='text' onChange={(e) => setTitle(e.target.value)} value={title} required className="inputField" />
+      </div>
+      <div>
+        <label htmlFor='description'>Description:</label>
+      </div>
+      <div>
+        <textarea id='description' type='text' onChange={(e) => setDescription(e.target.value)} value={description} required className="inputField" />
+      </div>
+      <div>
+        <button type="submit" className="submitButton">Submit</button>
+        <button type="button" onClick={handleCancelClick} className="cancelButton">Cancel</button>
+      </div>
 
-      </form>
-    </div>
+    </form>
 
   );
 
