17479e310b9163a73cd7ec2149a84bf8cd9a800c
Huan4Ai <aihuan96@gmail.com>
2021-12-30 16:58:09 -0500

    delete answer modal styled

--- frontend/src/components/DeleteAnswerPage/DeleteAnswer.js
+++ frontend/src/components/DeleteAnswerPage/DeleteAnswer.js
@@ -1,6 +1,5 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { deleteAnswer } from "../../store/answer";
-import { useHistory } from "react-router";
 import "./DeleteAnswer.css"
 
 function DeleteAnswer({ singleAnswer, onClose }) {
@@ -11,14 +10,27 @@ function DeleteAnswer({ singleAnswer, onClose }) {
     e.preventDefault();
     dispatch(deleteAnswer(answerId));
   }
+
+  const handleCancelClick = (e) => {
+    e.preventDefault();
+    onClose();
+  };
+
   return (
 
-    <form onSubmit={handleDelete}>
-      <button type="submit" className="deleteButtonOnAnswer">Delete</button>
-    </form>
+    <div className="deleteAnswerContainer">
+      <form onSubmit={handleDelete}>
+        <div>
+          <h2 id="deleteAnswerHeader">Are you sure you want to delete this answer?</h2>
+        </div>
+        <div className="deleteAnswerButtons">
+          <button type="button" onClick={handleCancelClick} id="cancelDeleteButton">Cancel</button>
+          <button type="submit" id="deleteButton">Delete</button>
+        </div>
+      </form>
+    </div>
 
   );
-
 
 
 }
